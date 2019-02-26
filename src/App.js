aab4db0bcac2429d243b00a09a93d9f71a6f39ae
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-25 22:31:09 -0500

    add function to pick random meme image from state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import MemeGenerator from "./components/MemeGenerator";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Meme Generator</h1>
+      <div>
+        <Header />
+        <MemeGenerator />
       </div>
     );
   }
